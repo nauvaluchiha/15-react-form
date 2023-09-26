53a45860388c183601c95a4e9bf5595adca71c6f
nauvaluchiha <142927902+nauvaluchiha@users.noreply.github.com>
2023-09-26 16:48:09 +0700

    Update product-table.tsx

--- src/pages/product/components/product-table.tsx
+++ src/pages/product/components/product-table.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Cell, ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import React from "react";
 import {
   Table,
   TableBody,
@@ -8,6 +8,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Link } from "react-router-dom";
 
 interface CellComponentProps<TData> {
   cell: Cell<TData, unknown>;
@@ -17,61 +18,70 @@ interface DataTableProps<TData, TValue> {
   data: TData[];
 }
 
-export const ProductTable = React.memo(<TData, TValue>({
-  columns,
-  data,
-}: DataTableProps<TData, TValue>) => {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+export const ProductTable = React.memo(
+  <TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) => {
+    const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+    });
 
-  return (
-    <div className="border-[1px] border-black rounded-lg">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} className="border-[1px] border-gray-200 text-center rounded-lg">
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => {
-              return (
-                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                  {row.getVisibleCells().map((cell) => {
-                      return (
-                        <CellComponent key={cell.id} cell={cell} />
-                      );
+    return (
+      <div>
+        <div className="border-[1px] border-black rounded-lg">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead
+                        key={header.id}
+                        className="border-[1px] border-gray-200 text-center rounded-lg"
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
+                    );
                   })}
                 </TableRow>
-              );
-            })
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
-});
-
-
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => {
+                  return (
+                    <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                      {row.getVisibleCells().map((cell) => {
+                        return <CellComponent key={cell.id} cell={cell} />;
+                      })}
+                    </TableRow>
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} className="h-24 text-center">
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="text-center mt-4">
+          <h2 className="text-base font-mono">
+            <strong>API</strong>:{" "}
+            <Link to="https://mocki.io/v1/cd86a97f-1f9c-4828-853b-4085dac3aff9" className="text-blue-400">
+              https://mocki.io/v1/cd86a97f-1f9c-4828-853b-4085dac3aff9
+            </Link>
+          </h2>
+          <h5 className="mt-12">Made by <span className="text-red-500">Naufaln</span> with <span className="underline">React, Tailwind, Shadcn, React Hook Form, Zod, TypeScript</span></h5>
+        </div>
+      </div>
+    );
+  }
+);
 
 function CellComponent<TData>({ cell }: CellComponentProps<TData>) {
   return (
